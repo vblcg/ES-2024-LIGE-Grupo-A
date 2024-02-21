29a78f1cd35bf98d6e4e955590290a70765f7c33
pedroferreira <pmacf@iscte-iul.pt>
2024-02-21 20:00:04 +0000

    testing configs

--- teste/src/Folder1/App.java
+++ teste/src/Folder1/App.java
@@ -1,6 +1,14 @@
 package Folder1;
 public class App {
+    public static int increment(int number) {
+        for (int ii = 0; ii < 1000; ii++) {
+            number++;
+            System.out.println(number);
+        }
+        return number;
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        App.increment(0);
     }
 }
