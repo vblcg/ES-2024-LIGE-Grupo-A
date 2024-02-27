c51db29bbd7cfe110906e0b52822baffd3484f7b
pedroferreira <pmacf@iscte-iul.pt>
2024-02-27 11:35:33 +0000

    deleteHelloTwitter

---
+++ Projeto/src/Folder 2/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String args[]) {
+        int i = 0;
+        for (int ii = 0; ii < 10; ii++) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
