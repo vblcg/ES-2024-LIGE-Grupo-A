f61548f1ebaae6dacf86eba2697498658e0ae07e
Nuno Lages <nuno.lages.lourenco@gmail.com>
2024-03-06 16:06:36 +0000

    ALteração na tabela do tabulator

--- maven/teste/src/main/java/iscte/lerHorario.java
+++ maven/teste/src/main/java/iscte/lerHorario.java
@@ -8,18 +8,20 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class lerHorario {
-    private DefaultTableModel tableModel;
 
     static String[] colunas = {"Curso", "UC", "Turno", "Turma", "Inscritos no Turno", "Dia da Semana", "Hora Início da Aula", "Hora Fim da Aula", "Data da aula", "Caracteristicas da sala pedida para a aula", "Sala atribuida a aula"};
 
-    public static String[] getStringArray() {
-        return colunas;
-    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+         try (CSVReader csvReader = new CSVReaderBuilder(new FileReader("FICHEIRO.csv")).withSkipLines(1).build()) {
+            
+         }
 
     }
 
