9e95b4260fee49395b8eed6e5ecf05e2d97ab176
Nuno Lages <nuno.lages.lourenco@gmail.com>
2024-03-06 15:16:21 +0000

    dependency

--- maven/teste/src/main/java/iscte/lerHorario.java
+++ maven/teste/src/main/java/iscte/lerHorario.java
@@ -1,5 +1,15 @@
 package iscte;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
 public class lerHorario {
+    
+    
+    public static void main(String[] args) {
+
+    }
+
+
     
 }
