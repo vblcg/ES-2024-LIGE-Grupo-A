bdc81d932014291966f94822c0c535529b3a383c
pedroferreira <pmacf@iscte-iul.pt>
2024-03-06 22:07:44 +0000

    upload_v1.3 - csv to json

--- maven/teste/src/main/java/iscte/App.java
+++ maven/teste/src/main/java/iscte/App.java
@@ -1,30 +1,13 @@
 package iscte;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
         File[] fileHolder = new File[1];
-
-        userUploadFile.getFileFromUser((selectedFile) -> {
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-            fileHolder[0] = new File(selectedFile.getAbsolutePath()); 
-        });
-
-        if (fileHolder[0] != null) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileHolder[0]))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line); // Print each line of the file
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("No file selected.");
-        }
+        String jsonFile = "output.json";
+        userUploadFile.getFileFromUser((selectedFile) -> {fileHolder[0] = new File(selectedFile.getAbsolutePath()); });
+        csvToJson ctj = new csvToJson(fileHolder, jsonFile);
+        ctj.extract();
     }
 }
