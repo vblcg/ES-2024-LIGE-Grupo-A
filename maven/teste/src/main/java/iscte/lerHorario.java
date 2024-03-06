43e5130a1034dab83d379acf45806968ba715d7c
Nuno Lages <nuno.lages.lourenco@gmail.com>
2024-03-06 18:25:26 +0000

    teste dos dados a serem lidos de um ficheiro csv

--- maven/teste/src/main/java/iscte/lerHorario.java
+++ maven/teste/src/main/java/iscte/lerHorario.java
@@ -2,30 +2,31 @@ package iscte;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class lerHorario {
 
-    String csvFile = "yourfile.csv";
+    static String csvFile = "maven/teste/HorarioDeExemplo.csv";
 
     static String[] colunas = {"Curso", "UC", "Turno", "Turma", "Inscritos no Turno", "Dia da Semana", "Hora Início da Aula", "Hora Fim da Aula", "Data da aula", "Caracteristicas da sala pedida para a aula", "Sala atribuida a aula"};
 
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-         try (CSVReader csvReader = new CSVReaderBuilder(new FileReader("FICHEIRO.csv")).withSkipLines(1).build()) {
-            
-         }
+    public static void main(String[] args) throws FileNotFoundException, IOException, CsvValidationException {
+        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withSkipLines(1).build()) {
+
+            //teste para verificar a leitura correta do ficheir csv
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                System.out.println("Row: " + String.join(", ", nextRecord));
+            }
 
     }
+}
 
 
     
