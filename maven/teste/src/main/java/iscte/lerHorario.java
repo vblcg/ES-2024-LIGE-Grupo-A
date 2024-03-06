37a525acc75a5300a736e3c73c7fa73d20d8f8bb
JoaoLourenco-iscte <jtvfl@iscte-iul.pt>
2024-03-06 15:30:40 +0000

    Created HTML folder

--- maven/teste/src/main/java/iscte/lerHorario.java
+++ maven/teste/src/main/java/iscte/lerHorario.java
@@ -3,7 +3,20 @@ package iscte;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class lerHorario {
+    private DefaultTableModel tableModel;
+
+
+    String[] colunas = {"Curso", "UC", "Turno", "Turma", "Inscritos no Turno", "Dia da Semana", "Hora Início da Aula", "Hora Fim da Aula", "Data da aula", "Caracteristicas da sala pedida para a aula", "Sala atribuida a aula"};
+    
+    tableModel = new DefaultTableModel(colunas);
     
     
     public static void main(String[] args) {
