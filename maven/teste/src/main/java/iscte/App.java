ac2c16dd2fe38b305dda3b2dcf1fde636f78ffb4
pedroferreira <pmacf@iscte-iul.pt>
2024-03-07 21:14:54 +0000

    upload_v1.8

--- maven/teste/src/main/java/iscte/App.java
+++ maven/teste/src/main/java/iscte/App.java
@@ -5,8 +5,8 @@ public class App {
     public static void main(String[] args) {
         File[] fileHolder = new File[1];
         String jsonFile = "output.json";
-        userUploadFile.getFileFromUser((selectedFile) -> {fileHolder[0] = new File(selectedFile.getAbsolutePath()); });
-        csvToJson extractorCsvToJson = new csvToJson(fileHolder, jsonFile);
-        extractorCsvToJson.extract();
+        userUploadFile.getFileFromUser((selectedFile) -> {fileHolder[0] = new File(selectedFile.getAbsolutePath());});
+        extract extractorCsvToJson = new extract(fileHolder, jsonFile);
+        extractorCsvToJson.readCsvUsingBufferReader();
     }
 }
