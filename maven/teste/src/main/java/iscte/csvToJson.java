2676601560de26265a97ebc10dc0c1481766fdf4
vascogaspar <vblcg@iscte-iul.pt>
2024-03-07 14:33:56 +0000

    upload_v1.5

--- maven/teste/src/main/java/iscte/csvToJson.java
+++ maven/teste/src/main/java/iscte/csvToJson.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -24,12 +25,17 @@ public class csvToJson{
         if (this.holder[0] != null) {
             try (CSVReader reader = new CSVReader(new FileReader(this.holder[0]))) {
                 List<String[]> csvData = reader.readAll();
+                //ArrayList<String[]> csvDataToWrite = new ArrayList<>();
+                //for (int i =0; i< csvData.size(); i++){
+                //    csvDataToWrite.get(i).add(csvData.get(i)[0].split(";"));
+                //}
 
                 String[] headers = csvData.remove(0);
+                String [] splitedHeaders = headers[0].split(";");
                 Gson gson = new GsonBuilder().setPrettyPrinting().create();
                 FileWriter writer = new FileWriter(outputFile);
-                gson.toJson(csvData, writer);
-
+                gson.toJson(csvData.get(0)[0], writer);
+                
                 writer.close();
             } catch (IOException | CsvException e) {
                 e.printStackTrace();
