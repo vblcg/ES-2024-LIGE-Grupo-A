92786abd88a564ac6925c1515374acecd82360d8
pedroferreira <pmacf@iscte-iul.pt>
2024-03-14 20:55:59 +0000

    ponto3_v2.1.1

--- maven/teste/src/main/java/iscte/App.java
+++ maven/teste/src/main/java/iscte/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         File[] fileHolder = new File[1]; 
         String jsonFile = "output.json";
-        XamppFileManager.saveToFileRoot(new File("SalasDeAulaPorTipospontoDeSala.html"));
+        XamppFileManager.saveToFileRoot(new File("SalasDeAulaPorTiposDeSala.html"));
         UserUploadFile uploadFile = new UserUploadFile(fileHolder, jsonFile); 
         uploadFile.setVisible(true); 
     }
