a98ca316b6e9f68575982e2a0c029e4cfe060230
Nuno Lages <nuno.lages.lourenco@gmail.com>
2024-03-15 18:29:11 +0000

    ponto2-v1.12

--- maven/teste/src/test/js/horarioTest.js
+++ maven/teste/src/test/js/horarioTest.js
@@ -1,27 +1,24 @@
-const assert = require('chai').assert;
-const { toggleColumn, minMaxFilterFunction } = require('../../main/HTML/horario');
+const test = require('ava');
+const { minMaxFilterFunction } = require('../../main/HTML/horario');
 
+test('minMaxFilterFunction deve retornar verdadeiro para um intervalo válido', t => {
+    const headerValue = { start: 10, end: 20 };
+    t.true(minMaxFilterFunction(headerValue, 15, {}, {}));
+});
 
-describe('minMaxFilterFunction', function() {
-    it('Deve retornar true para um intervalo válido', function() {
-        const headerValue = { start: 10, end: 20 };
-        assert.isTrue(minMaxFilterFunction(headerValue, 15, {}, {}));
-    });
+test('minMaxFilterFunction deve retornar falso para um intervalo inválido', t => {
+    const headerValue = { start: 10, end: 20 };
+    t.false(minMaxFilterFunction(headerValue, 5, {}, {})); // Valor fora do intervalo
+    t.false(minMaxFilterFunction(headerValue, 25, {}, {})); // Valor fora do intervalo
+});
 
-    it('Deve retornar falso para um intervalo inválido', function() {
-        const headerValue = { start: 10, end: 20 };
-        assert.isFalse(minMaxFilterFunction(headerValue, 5, {}, {})); // Value outside range
-        assert.isFalse(minMaxFilterFunction(headerValue, 25, {}, {})); // Value outside range
-    });
+test('minMaxFilterFunction deve retornar verdadeiro se o mínimo e o máximo não estiverem especificados', t => {
+    const headerValue = { start: '', end: '' };
+    t.true(minMaxFilterFunction(headerValue, 15, {}, {}));
+});
 
-    it('Deve retornar verdadeiro se os mínimo e o máximo não estiverem especificados', function() {
-        const headerValue = { start: '', end: '' };
-        assert.isTrue(minMaxFilterFunction(headerValue, 15, {}, {}));
-    });
-
-    it('Deve retornar true se o valor da coluna for null ou undefined', function() {
-        const headerValue = { start: 10, end: 20 };
-        assert.isTrue(minMaxFilterFunction(headerValue, null, {}, {}));
-        assert.isTrue(minMaxFilterFunction(headerValue, undefined, {}, {}));
-    });
+test('minMaxFilterFunction deve retornar verdadeiro se o valor da coluna for nulo ou indefinido', t => {
+    const headerValue = { start: 10, end: 20 };
+    t.true(minMaxFilterFunction(headerValue, null, {}, {}));
+    t.true(minMaxFilterFunction(headerValue, undefined, {}, {}));
 });
