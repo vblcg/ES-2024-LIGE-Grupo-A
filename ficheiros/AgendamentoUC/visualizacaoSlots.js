aa2f4a11f73589ccfbd26c8734205492dc4be782
pedroferreira <pmacf@iscte-iul.pt>
2024-04-23 21:29:19 +0100

    alterações

--- ficheiros/AgendamentoUC/visualizacaoSlots.js
+++ ficheiros/AgendamentoUC/visualizacaoSlots.js
@@ -130,10 +130,8 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then((data) => {
       horario = data;
-      console.log(cursoData);
-      console.log(horario);
+      allAccepted = cursoData.length;
       horario = cursoData.concat(horario);
-      console.log(horario);
 
       table = new Tabulator("#tabela-dados", {
         height: "650px",
@@ -301,9 +299,10 @@ document.addEventListener("DOMContentLoaded", function () {
                   cell.getElement().innerHTML = "";
                 }
               } else if (e.target.classList.contains("modify-btn")) {
-                
+                  aulaAMudar = cell.getRow().getData();
+                  $('#myModal').modal('show');
               } else if (e.target.classList.contains("delete-btn")) {
-                var confirmacao = confirm("Tem a certeza que quer apagar a aula? Não há volta a dar depois se der merda já bateste");
+                var confirmacao = confirm("Tem a certeza que quer apagar a aula?");
                 if (confirmacao) {
                   var rowData = cell.getRow().getData(); 
                   var rowIndex = cell.getRow().getIndex(); 
@@ -1552,5 +1551,3 @@ if (container) {
   return 0; // Retorna 0 para tratar o erro
 }
 }
-
-
